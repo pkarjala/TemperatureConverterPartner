ba8ad9642c9b163acb4c48c00aff9ed805525298
Patrick Karjala <pkarjala@gmail.com>
2015-02-02 15:16:06 -1000

    Fix error checking.

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,25 +1,44 @@
+import com.sun.javafx.css.CssError;
+
 /**
  * Created by patrick on 2/2/15.
  */
 public class TemperatureConverter {
   public static void main(String[] args) {
     double celsius, fahrenheit = 0;
-    // If flag is -F for value is Fahrenheit
-    for( String value : args ){
-      System.out.println("Array Value: " + value);
+    // Check for present args
+    String firstArg = "0";
+    double secondArg = 0.0;
+    if( args.length > 0 ) {
+        firstArg = args[0];
+    } else {
+      System.out.println("No arguments provided.");
+      System.exit(1);
     }
-    if(args[0].equals("-F")) {
-      fahrenheit = Double.parseDouble(args[1]);
+    if (args.length > 1) {
+      try {
+        secondArg = Double.parseDouble(args[1]);
+      } catch(NumberFormatException f) {
+        System.err.println("Second argument " + args[1] + " must be a number.");
+        System.exit(1);
+      }
+    } else {
+      System.out.println("No second argument provided.");
+      System.exit(1);
+    }
+    if(firstArg.equals("-F")) {
+      fahrenheit = secondArg;
       celsius = (fahrenheit - 32) * 5/9;
       System.out.println(fahrenheit + " degrees Fahrenheit is equal to " + celsius + " degrees Celsius");
     }
-    else if (args[0].equals("-C")) {
-      celsius = Double.parseDouble(args[1]);
+    else if (firstArg.equals("-C")) {
+      celsius = secondArg;
       fahrenheit = (celsius * 9/5) + 32;
       System.out.println(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
     }
     else {
       System.out.println("Illegal first argument");
+      return;
     }
   }
 }
