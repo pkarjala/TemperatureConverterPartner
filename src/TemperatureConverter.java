c949f9a52cd66785705d0c2e737369fb8e20f85d
Patrick Karjala <pkarjala@hawaii.edu>
2015-02-03 21:03:58 -1000

    Final Tweaks.

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -3,36 +3,39 @@
  */
 public class TemperatureConverter {
   public static void main(String[] args) {
-    double celsius, fahrenheit = 0;
+    double celsius, fahrenheit;
     // Check for present args
     String firstArg = "0";
     double secondArg = 0.0;
-    if( args.length > 0 ) {
-        firstArg = args[0];
-    } else {
+    if (args.length > 0) {
+      firstArg = args[0];
+    }
+    else {
       System.out.println("No arguments provided.");
       System.exit(1);
     }
     if (args.length > 1) {
       try {
         secondArg = Double.parseDouble(args[1]);
-      } catch(NumberFormatException f) {
+      }
+      catch (NumberFormatException f) {
         System.err.println("Second argument " + args[1] + " must be a number.");
         System.exit(1);
       }
-    } else {
+    }
+    else {
       System.out.println("No second argument provided.");
       System.exit(1);
     }
-    if(firstArg.equals("-F")) {
+    if (firstArg.equals("-F")) {
       fahrenheit = secondArg;
-      celsius = (fahrenheit - 32) * 5/9;
+      celsius = (fahrenheit - 32) * 5 / 9;
       System.out.println(fahrenheit + " degrees Fahrenheit is equal to " + celsius + " degrees Celsius");
     }
     else if (firstArg.equals("-C")) {
       celsius = secondArg;
-      fahrenheit = (celsius * 9/5) + 32;
-      System.out.println(celsius + " degrees Celsius is equal to " +  fahrenheit + " degrees Fahrenheit.");
+      fahrenheit = (celsius * 9 / 5) + 32;
+      System.out.println(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
     }
     else {
       System.out.println("Illegal first argument");
